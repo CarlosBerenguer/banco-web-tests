4ba6a16377e358cdd5a982ea36bfbc373f183e92
CarlosBerenguer <carloshfb03@gmail.com>
2025-07-23 16:25:52 -0300

    adicinei um teste para validar o login invalido

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,12 +2,28 @@ describe('Login', () => {
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     //arrange
     cy.visit('http://localhost:4000')
+
     //act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
-    //assert
+    cy.contains('button','Entrar').click()
 
+    //assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  })
+
+  it('Login com senha inválida deve apresentar mensagem de erro', () => {
+    //arrange
+    cy.visit('http://localhost:4000')
+
+    //act
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('1234567')
+    cy.get('#login-section > .btn').click()
+
+    //assert
+    cy.get('.toast')
+      .should('be.visible')
+      .should('have.text', 'Erro no login. Tente novamente.')
   })
 })
